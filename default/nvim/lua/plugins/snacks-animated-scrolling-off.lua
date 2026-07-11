718ec0c3ff69145bfb2721da2c86aee0b920df8d
Britt Carter <nukelheadmp@gmail.com>
2026-07-11 00:37:48 -0500

    Restored neovim explorer default focus behavior

--- default/nvim/lua/plugins/snacks-animated-scrolling-off.lua
+++ default/nvim/lua/plugins/snacks-animated-scrolling-off.lua
@@ -1,20 +1,20 @@
 return {
   "folke/snacks.nvim",
   opts = {
-    picker = {
-      sources = {
-        explorer = {
-          actions = {
-            -- Modify explorer focus to update cwd
-            explorer_focus = function(picker)
-              picker:set_cwd(picker:dir())
-              picker:find()
-              vim.cmd.tcd(picker:cwd())
-            end,
-          },
-        },
-      },
-    },
+    --picker = {
+    --  sources = {
+    --    explorer = {
+    --      actions = {
+    --        -- Modify explorer focus to update cwd
+    --        explorer_focus = function(picker)
+    --          picker:set_cwd(picker:dir())
+    --          picker:find()
+    --          vim.cmd.tcd(picker:cwd())
+    --        end,
+    --      },
+    --    },
+    --  },
+    --},
     scroll = {
       enabled = false, -- Disable scrolling animations
     },
---
+++ migrations/1783747967.sh
@@ -0,0 +1,2 @@
+echo "Update neovim config"
+cp ~/.local/share/omadora/default/nvim/lua/plugins/snacks-animated-scrolling-off.lua ~/.config/nvim/lua/plugins/
