04f25d1e5dace16e817b2f200ee8a9f52507e081
Britt Carter <nukelheadmp@gmail.com>
2026-07-11 01:38:43 -0500

    Install and enable lightdm

---
+++ migrations/1783750009.sh
@@ -0,0 +1,11 @@
+echo "Install lightdm package"
+sudo dnf install -y lightdm
+
+echo "Copying default background"
+cp ~/.local/share/omadora/themes/rose-pine-darker/backgrounds/0-default.png /usr/share/backgrounds/default.png
+
+echo "Copying lightdm config"
+sudo cp ~/.local/share/omadora/default/lightdm/slick-greeter.conf /etc/lightdm/
+
+echo "Enable lightdm"
+sudo systemctl enable lightdm.service
