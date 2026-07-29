d65a32d7ba2d1bfcedc97ef161259b71d02881f1
Britt Carter <nukelheadmp@gmail.com>
2026-07-29 18:24:12 -0500

    Add window rule for snes9x-gtk

--- config/hypr/windows.lua
+++ config/hypr/windows.lua
@@ -1,23 +1,30 @@
 -- Add custom window config in this file.
 
 hl.window_rule({
-	match = {
-		class = "org.qbittorrent.qBittorrent",
-	},
-	float = true,
+  match = {
+    class = "org.qbittorrent.qBittorrent",
+  },
+  float = true,
 })
 hl.window_rule({
-	match = {
-		class = "org.keepassxc.KeePassXC",
-	},
-	float = true,
+  match = {
+    class = "org.keepassxc.KeePassXC",
+  },
+  float = true,
 })
 hl.window_rule({
-	match = {
-		class = "nestopia",
-	},
-	float = true,
-	idle_inhibit = "focus",
+  match = {
+    class = "nestopia",
+  },
+  float = true,
+  idle_inhibit = "focus",
+})
+hl.window_rule({
+  match = {
+    class = "snes9x-gtk",
+  },
+  float = true,
+  idle_inhibit = "focus",
 })
 --windowrule = float on, match:title ^(RollerCoaster Tycoon)$
 --windowrule = float on, match:class ^(org\.gnome\.Evolution)$, match:title ^(Compose Message)$
