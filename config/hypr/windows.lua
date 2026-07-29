98e6424afc6e4ef6da0c49c12c1d2516f67520b5
Britt Carter <nukelheadmp@gmail.com>
2026-07-29 00:54:32 -0500

    Added Nestopia window rules

---
+++ config/hypr/windows.lua
@@ -0,0 +1,24 @@
+-- Add custom window config in this file.
+
+hl.window_rule({
+	match = {
+		class = "org.qbittorrent.qBittorrent",
+	},
+	float = true,
+})
+hl.window_rule({
+	match = {
+		class = "org.keepassxc.KeePassXC",
+	},
+	float = true,
+})
+hl.window_rule({
+	match = {
+		class = "nestopia",
+	},
+	float = true,
+	idle_inhibit = "focus",
+})
+--windowrule = float on, match:title ^(RollerCoaster Tycoon)$
+--windowrule = float on, match:class ^(org\.gnome\.Evolution)$, match:title ^(Compose Message)$
+--windowrule = size 900 700, match:class ^(org\.gnome\.Evolution)$, match:title ^(Compose Message)$
---
+++ default/hypr/apps/nestopia.lua
@@ -0,0 +1,7 @@
+hl.window_rule({
+  name = "Nestopia",
+  match = {
+    class = "nestopia",
+  },
+  float = true,
+})
