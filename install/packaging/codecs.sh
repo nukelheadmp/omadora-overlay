14420e2721f973cd2a7ebef347f4b78d34319292
Britt Carter <nukelheadmp@gmail.com>
2026-08-11 00:08:26 -0500

    Removed mesa drivers from codecs install

--- install/packaging/codecs.sh
+++ install/packaging/codecs.sh
@@ -1,4 +1,2 @@
 # Install codecs and drivers
-sudo dnf swap -y mesa-va-drivers mesa-va-drivers-freeworld
-sudo dnf swap -y mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
 sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
